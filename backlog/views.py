a6c5cf35f06f7b72cff652736f65815e36e0da4e
josese7 <segovia.jose2196@gmail.com>
2022-06-21 17:12:54 -0400

    Sprint 5 21-06

--- backlog/views.py
+++ backlog/views.py
@@ -239,7 +239,7 @@ class CreateComentarioUs( LoginRequiredMixin, CreateView):
         self.object = None
         form_class = self.get_form_class()
         form = self.get_form(form_class)
-        
+
 
         if form.is_valid():
             return self.form_valid(form)
